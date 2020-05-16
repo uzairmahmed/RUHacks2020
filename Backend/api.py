a892cab6a7af33308c0c26e2b4d2488077946e92
Varghese Rony George <varghese.rony.george@gmail.com>
2020-05-16 15:26:06 -0400

    Added CORS to Flask API

--- Backend/api.py
+++ Backend/api.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def home():
