2e67cff1aad0e3c6455f55ab9f61224cae4ca9d0
Varghese Rony George <varghese.rony.george@gmail.com>
2020-05-17 09:00:36 -0400

    Updates

---
+++ precache-manifest.5a12c3aa8ddcc513cf266c17e34333bd.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6903f84f00c971538106cc00a90fa291",
+    "url": "/RUHacks2020/index.html"
+  },
+  {
+    "revision": "ecc4b3c9f6e9256c315c",
+    "url": "/RUHacks2020/static/css/main.5d3a25d5.chunk.css"
+  },
+  {
+    "revision": "b495459220df0dc6683b",
+    "url": "/RUHacks2020/static/js/2.5ca36819.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RUHacks2020/static/js/2.5ca36819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecc4b3c9f6e9256c315c",
+    "url": "/RUHacks2020/static/js/main.7b2f3cda.chunk.js"
+  },
+  {
+    "revision": "5579b42d712011ccf094",
+    "url": "/RUHacks2020/static/js/runtime-main.2c72f490.js"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/RUHacks2020/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "0eb8cedcedefe0b04a200dc5c9602f4a",
+    "url": "/RUHacks2020/static/media/fa-brands-400.0eb8cedc.svg"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/RUHacks2020/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/RUHacks2020/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/RUHacks2020/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/RUHacks2020/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9b4c53aa538b8e0c4def479a43a8acf1",
+    "url": "/RUHacks2020/static/media/fa-regular-400.9b4c53aa.svg"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/RUHacks2020/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/RUHacks2020/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/RUHacks2020/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/RUHacks2020/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "3719793fcebc92ff6642bf287db11b16",
+    "url": "/RUHacks2020/static/media/fa-solid-900.3719793f.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/RUHacks2020/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/RUHacks2020/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/RUHacks2020/static/media/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/RUHacks2020/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/RUHacks2020/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/RUHacks2020/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/RUHacks2020/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/RUHacks2020/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
